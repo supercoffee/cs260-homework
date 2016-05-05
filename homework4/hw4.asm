ece917f8474824b4b19bdb8fcc2123927d4a8e70
Benjamin Daschel <coffeemaxed@gmail.com>
2016-05-04 20:55:14 -0700

    homework 4 reverse recursive string printing workinggit add hw4.asm

--- homework4/hw4.asm
+++ homework4/hw4.asm
@@ -1,5 +1,5 @@
 .data
-name: .asciiz "Benjamin"
+name: .asciiz "\nBenjamin"
 
 .text
 .globl main
@@ -12,33 +12,29 @@ main:
     li $v0, 10
     syscall
 
-
 print_r:
   # move stack pointer down and save registers
   addi $sp, $sp, -8
   sw $a0, 0($sp)
   sw $ra, 4($sp)
   # load src address from a0
-  lb $a0, ($a0)
+  lb $t0, ($a0)
+
+  # return if loaded character is terminator
+  beqz $t0, base_case
+  addi $a0, $a0, 1 # increment string pointer by 1 byte
+  jal print_r
+  addi $a0, $a0, -1 # decrement string pointer to restore it to what
+  # the current execution scope should be looking at
 
   # print a character
+  lb $a0, ($a0) # load character from address
   li $v0, 11
   syscall
 
+  base_case:
   # reset registers and prepare to jump into hyperspace
   lw $ra, 4($sp)
   lw $a0, 0($sp)
   addi $sp, $sp, 8
   jr $ra
-
-  # load contents of src into register
-  # if contents == 0: stop recursion
-  # else increment src address
-  # save current src address to stack
-  # subtract stack pointer
-  # call print_r
-
-  # load address back from stack
-  # load contents into register
-  # print character
-  # return to previous call
